20231c9955ae8c4ccbed15dd51095743a8db0681
Daisy Martinez <daisymar@hawaii.edu>
2023-09-23 20:54:53 -1000

    wods

---
+++ FakeWODs/SmartPhoneProducts1_1/products_data.js
@@ -0,0 +1,12 @@
+
+//Copy and paste the variables
+    var name1 = "HTC"; 
+    var price1 = 40.00; 
+    var image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"; 
+
+    var name2 = "Apple";
+    var price2 = 75.00;
+    var image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+
+//ig this will print to the console
+    console.log("Product variables loaded...");
