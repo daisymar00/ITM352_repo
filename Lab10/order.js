670baf7ec3a207a88df13988974a3a23394cb2c3
Daisy Martinez <daisymar@hawaii.edu>
2023-10-29 18:31:42 -1000

    Lab10 part 1

---
+++ Lab10/order.js
@@ -0,0 +1,38 @@
+function updateQuantityMessage(textbook) {
+    let quantityMessage = document.getElementById('qty_textbook_message');
+
+    // Validate the quantity entered
+    let validMessage = validateQuantity(Number(textbook.value));
+
+    // If there are validation erros, display an error message
+    if (validMessage !== "") {
+        quantityMessage.innerHTML = validMessage;
+    } else {
+        quantityMessage.innerHTML = textbook.value;
+    }
+}
+
+function validateQuantity(quantity) {
+    let errorMessage = "";
+
+    switch (true) {
+        case isNaN(quantity):
+            errorMessage = "Not a number. Please enter a non negatibe quantity to order.";
+            break;
+        case quantity < 0 && Number.isInteger(quantity):
+            errorMessage = "Negative inventory and not an integer. Please enter a non negative quantity to order.";
+            break;
+        case quantity == 0:
+            errorMessage = "Enter an integer foo."
+        case quantity < 0:
+            errorMessage = "Negative inventory. Please enter a non negative quantity to order.";
+            break;
+        case Number.isInteger(quantity):
+            errorMessage = "Not an integer. Please enter a non-negative quantity to order.";
+            break;
+        default:
+            errorMessage = ""; // No errors
+            break;
+    }
+    return errorMessage;
+}
